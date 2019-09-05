37ab4f562c76753e7fc03ac4a163ab095d3e1ef7
Róbert Arnarsson <robert19@ru.is>
2019-09-05 11:20:03 +0000

    The code for max_int

--- assignemt5.py
+++ assignemt5.py
@@ -3,4 +3,14 @@
 # I Enter: (1,5,3,19,2,70,71,-1)
 # I should get "The maximum is 71"
 
-
+num_int = int(input("Input a number: "))    # Do not change this line
+max_int = 0
+# Fill in the missing code
+while num_int >= 0:
+    num_int = int(input("Input a number: "))
+    # Here am I saying that if the number that was entered 
+    # the second time is higher then the first
+    # then it becomes the max_int
+    if num_int > max_int:
+        max_int = num_int
+print("The maximum is", max_int)    # Do not change this line
